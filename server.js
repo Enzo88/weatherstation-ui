67414eaedcbb5f4fbcadfedcde0e99f87a07ee72
Vincenzo Pimpinella <vincenzo.pimpinella@autosoft.it>
2019-08-09 17:42:56 +0200

    upd

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/Weather-ui'));
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/Weather-ui/index.html'));
 });
+
+app.post('/api/sendWeatherData/:deviceId', (req, res) => {
+    return res.send('Received a POST HTTP method'+req.params.deviceId);
+  });
+
+app.get('/api/getCurrentWeather/:deviceId', (req, res) => {
+    return res.send('Received a POST HTTP method'+req.params.deviceId);
+  });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
